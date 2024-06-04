4f452c62c63a5925f778c3669e80e0dd2e375b43
DOAN MINH TU <minhtu.workmail@gmail.com>
2024-06-04 15:07:07 +0700

    Final draft

--- pages/Render_Output_File.py
+++ pages/Render_Output_File.py
@@ -24,7 +24,15 @@ with st.sidebar:
 with st_stdout("code",TerminalOutput, cache_data=True), st_stderr("code",LoggingOutput, cache_data=True):
     template_selection_col, data_selection_col = st.columns([3, 3])
 
-
+    if 'selected_template_sets' not in st.session_state:
+        st.session_state['selected_template_sets'] = []
+        
+    if 'context_data_list' not in st.session_state:
+        st.session_state['context_data_list'] = []
+        
+        
+    selected_template_sets = []
+    context_data_list = []
     ##########Template column management
     template_path = os.path.normpath(os.path.join(
                                                 os.path.dirname(os.path.abspath(__file__)),
@@ -70,7 +78,6 @@ with st_stdout("code",TerminalOutput, cache_data=True), st_stderr("code",Logging
 
 
         ####Generate context data
-        context_list = list()
         st.header("List context data")
         for selected_bid in  dynamic_filters["BID_INFO"].filter_df().to_dict('records'):
             # st.write(selected_bid)
@@ -80,22 +87,41 @@ with st_stdout("code",TerminalOutput, cache_data=True), st_stderr("code",Logging
                     context[key] = value
                 for key,value in selected_owner.items():
                     context[key] = value
-                with st.popover(context['E_TBMT']):
-                    st.write(context)
-
+                context_data_list.append(context)
+                
+    with st.popover("Preview before render"):
+        st.write(selected_template_sets)
+        for context in context_data_list:
+            st.write(context)
     from docxtpl import DocxTemplate
 
-    if st.button("render"):
-        for context in context_list:
-            for template_set in selected_template_sets:
-                template_file_list = dir_element_list(os.path.join(template_path,template_set),"file")
-                for template in template_file_list:
-                    template_file_path = os.path.normpath(
-                                            os.path.join(
-                                                template_path,
-                                                template_set , 
-                                                template)
+    
+    if st.button(":star2: :blue[**RENDER PROJECT FILE**]", use_container_width=True):
+        with st.status("Exporting data..."):
+            for context in context_data_list:
+                st.write("Processing data {}...".format(context['E_TBMT']))
+                for template_set_name in selected_template_sets:
+                    
+                    output_dir = CREATE_EXPORT_DIR(os.path.join('./',
+                                                            'output',
+                                                            context['E_TBMT'],
+                                                            template_set_name)
                                             )
-                template_object = DocxTemplate(template_file_path)
-                template_object.render(context)
-                template_object.save("test.docx")
+                    st.write("Creating folder {}...".format(output_dir))
+                    template_file_list = dir_element_list(os.path.join(template_path,template_set),"file")
+                    for template_name in template_file_list:
+                        template_file_path = os.path.normpath(
+                                                os.path.join(
+                                                    template_path,
+                                                    template_set_name , 
+                                                    template_name)
+                                                )
+                        template_object = DocxTemplate(template_file_path)
+                        template_object.render(context)
+                        output_file_name = os.path.join(output_dir,
+                                                        template_name)
+                        
+                        
+                        st.write("Creating fiile  {}...".format(os.path.abspath(output_file_name)))
+                        template_object.save(os.path.abspath(output_file_name))
+            st.write("Done")
